1353fe04b07d45493e9db0091eea58ab6d9f6d3e
Anton <b0rn2rage@bk,ru>
2020-07-23 21:22:27 +0300

    Перенос методов из main_page в base_page. Добавление двух новых тестов в test_product_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,25 +4,53 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        #self.browser.implicitly_wait(timeout)
+        # self.browser.implicitly_wait(timeout)
 
-    def open(self):
-        self.browser.get(self.url)
+    def go_to_login_page(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
 
     def is_element_present(self, how, what):
+        # Проверка наличия элемента на странице
         try:
             self.browser.find_element(how, what)
         except (NoSuchElementException):
             return False
         return True
 
+    def is_not_element_present(self, how, what, timeout=4):
+        # Проверка того, что элемент отсутствует на странице
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            # Если случается TimeoutException (элемента нет на странице), то тест успешно пройдет - вернется True
+            return True
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        # Проверка, что элемент исчезает со страницы
+        try:
+            # Третий аргумент '1' говорит о том, что webdriver делает запросы каждую секунду
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            # В течение 4 секунд проверяет наличие элемента на странице. Если элемент не исчез, то False и тест провален
+            return False
+        return True
+
+    def open(self):
+        self.browser.get(self.url)
+
     def solve_quiz_and_get_code(self):
+        # Прохождение капчи
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
         answer = str(math.log(abs((12 * math.sin(float(x))))))
@@ -36,20 +64,5 @@ class BasePage():
         except NoAlertPresentException:
             print("No second alert presented")
 
-    def is_not_element_present(self, how, what, timeout=4):
-        try:
-            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
-        except TimeoutException:
-            # Если случается TimeoutException (элемента нет на странице), то тест успешно пройдет - вернется True
-            return True
-        return False
-
-    def is_disappeared(self, how, what, timeout=4):
-        try:
-            # Третий аргумент '1' говорит о том, что webdriver делает запросы каждую секунду
-            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
-                until_not(EC.presence_of_element_located((how, what)))
-        except TimeoutException:
-            return False
-
-        return True
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,6 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
-
-
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
@@ -16,3 +12,8 @@ class ProductPageLocators():
     PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main h1')
     BASKET_PRICE = (By.CSS_SELECTOR, '.alertinner p strong')
     PRODUCT_PRICE = (By.CSS_SELECTOR, 'p.price_color')
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")

9b23e057119b28864578f314a48f10a8bbe7b4d1
Anton <b0rn2rage@bk,ru>
2020-07-27 23:24:20 +0300

    Добавление теста test_guest_cant_see_product_in_basket_opened_from_product_page

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -7,9 +7,12 @@ from selenium.webdriver.support import expected_conditions as EC
 
 class BasketPage(BasePage):
     def cart_should_be_empty_of_items(self):
-        assert self.is_not_element_present(*BasketPageLocators.PRODUCTS_IN_THE_CART), 'Корзина должна пустая,' \
+        """Негативная проверка. Чекает, что селектор, который появляется при добавлении
+        товара в корзину отсутствует на странице (т.к. товаров в корзине нет)"""
+        assert self.is_not_element_present(*BasketPageLocators.PRODUCTS_IN_THE_CART), 'Корзина должна быть пустая,' \
                                                                                       'т.к. в неё не добавляли товаров'
 
     def message_about_cart_is_empty(self):
-        '''Допилить реализацию'''
-        assert "Ваша корзина пуста" in self.is_element_present(*BasketPageLocators.EMPTY_CART), 'Корзина не пуста'
+        """Позитивная проверка. Чекает, что сообщение 'Ваша корзина пустая' отображается на странице"""
+        assert "Your basket is empty." in self.browser.find_element(
+            *BasketPageLocators.EMPTY_CART).text, 'Корзина не пуста'
--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 import pytest
 import time
 
@@ -65,3 +66,12 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+
+def test_guest_can_see_product_in_basket_opened_from_product_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_cart()
+    page.cart_should_be_empty_of_items()
+    page.message_about_cart_is_empty()
